3ff36fcce0ed79f2484a884c67b618ebb189393a
Rysertio <86291572+Rysertio@users.noreply.github.com>
2023-11-16 05:51:35 +0600

    Update infer.py

--- infer.py
+++ infer.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+from pydensecrf import densecrf
 
 def find_1d_repetitions(image, mask, main_direction, e=8):
     # Convert the mask to binary
@@ -52,24 +53,6 @@ def find_1d_repetitions(image, mask, main_direction, e=8):
 
     return optimal_path
 
-# Example usage
-image = cv2.imread('your_image.jpg')
-mask = cv2.imread('your_mask.jpg', cv2.IMREAD_GRAYSCALE)
-main_direction = np.array([1, 0])  # Example main direction vector
-
-optimal_path = find_1d_repetitions(image, mask, main_direction)
-
-# Visualize the result (optional)
-for i, j in optimal_path:
-    cv2.circle(image, (int(round(line_segment[i][0])), int(round(line_segment[i][1]))), 3, (0, 255, 0), -1)
-
-cv2.imshow('Optimal Path', image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-import cv2
-import numpy as np
-from pydensecrf import densecrf
-
 def displacement_assignment(image, mask, main_direction, optimal_path, initial_displacement):
     # Define constants
     theta_alpha = 10
@@ -117,20 +100,6 @@ def displacement_assignment(image, mask, main_direction, optimal_path, initial_d
 
     return result
 
-# Example usage
-image = cv2.imread('your_image.jpg')
-mask = cv2.imread('your_mask.jpg', cv2.IMREAD_GRAYSCALE)
-main_direction = np.array([1, 0])  # Example main direction vector
-initial_displacement = np.array([10, 0])  # Example initial displacement vector
-
-# Assuming optimal_path is obtained from the previous 1D repetitions step
-optimal_path = find_1d_repetitions(image, mask, main_direction)
-
-# Perform displacement assignment using CRF
-result = displacement_assignment(image, mask, main_direction, optimal_path, initial_displacement)
-import cv2
-import numpy as np
-
 def warp_image(image, displacement_field, mask):
     h, w = image.shape[:2]
 
@@ -173,22 +142,6 @@ def generate_seamless_animation(image, displacement_field_forward, displacement_
         seamless_animation.append(seamless_frame)
 
     return seamless_animation
-
-# Example usage
-image = cv2.imread('your_image.jpg')
-displacement_field_forward = load_displacement_field('displacement_field_forward.npy')  # Replace with actual loading function
-displacement_field_backward = load_displacement_field('displacement_field_backward.npy')  # Replace with actual loading function
-mask = cv2.imread('your_mask.jpg', cv2.IMREAD_GRAYSCALE) > 0  # Assuming the mask is a binary image
-alpha = lambda t: max(0, min(1, (t + 1) / 2))  # Piece-wise linear shift function
-
-seamless_animation = generate_seamless_animation(image, displacement_field_forward, displacement_field_backward, mask, alpha)
-
-# Display or save the generated animation frames as needed
-for frame in seamless_animation:
-    cv2.imshow('Seamless Animation', frame)
-    cv2.waitKey(100)  # Adjust the delay between frames as needed
-
-cv2.destroyAllWindows()
 
 def load_displacement_field(filename):
     try:
